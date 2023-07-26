8cede24621e898de93d679cc51d5761f86b59d2d
dimiten <122371764+dimiten@users.noreply.github.com>
2023-07-26 18:16:12 +0200

    Add files via upload

---
+++ nedelja_1/dz_02/rb_zad_2.py
@@ -0,0 +1,32 @@
+"""broj_u_indeksu = 105
+rb_zad_1 = broj_u_indeksu % 8 + 1 """
+
+"""Zadatak_2
+p -> (p v q)  """
+
+p = True
+q = True
+deo = p or q
+ceo = not p or deo
+print(p, q, ceo)
+
+p = True
+q = False
+deo = p or q
+ceo = not p or deo
+print(p, q, ceo)
+
+p = False
+q = True
+deo = p or q
+ceo = not p or deo
+print(p, q, ceo)
+
+p = False
+q = False
+deo = p or q
+ceo = not p or deo
+print(p, q, ceo)
+
+
+
